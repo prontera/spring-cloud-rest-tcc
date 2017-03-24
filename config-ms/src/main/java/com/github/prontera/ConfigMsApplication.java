ed76c1f9da4200e68d8aa2462b6c0934c1bd1112
chris <chiuchunkin@gmail.com>
2017-03-24 17:58:19 +0800

    添加gogs用于config server

---
+++ config-ms/src/main/java/com/github/prontera/ConfigMsApplication.java
@@ -0,0 +1,12 @@
+package com.github.prontera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigMsApplication.class, args);
+    }
+}
