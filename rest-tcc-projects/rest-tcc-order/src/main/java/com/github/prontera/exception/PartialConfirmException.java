257da671cd397eb71abffff922511cfe309677a5
Chris <chiuchunkin@gmail.com>
2020-01-16 23:52:29 +0800

    simplify projects

---
+++ rest-tcc-projects/rest-tcc-order/src/main/java/com/github/prontera/exception/PartialConfirmException.java
@@ -0,0 +1,31 @@
+package com.github.prontera.exception;
+
+import com.github.prontera.model.TccErrorResponse;
+
+/**
+ * @author Zhao Junjian
+ */
+public class PartialConfirmException extends RuntimeException {
+
+    private static final long serialVersionUID = -736692887311988416L;
+
+    public PartialConfirmException(String message) {
+        super(message);
+    }
+
+    public PartialConfirmException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public PartialConfirmException(Throwable cause) {
+        super(cause);
+    }
+
+    protected PartialConfirmException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+
+    public PartialConfirmException(String message, TccErrorResponse tccErrorResponse) {
+
+    }
+}
---
+++ rest-tcc-projects/rest-tcc-product/src/main/java/com/github/prontera/model/response/ReservationResponse.java
@@ -0,0 +1,32 @@
+package com.github.prontera.model.response;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.github.prontera.model.Participant;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * @author Zhao Junjian
+ */
+@Getter
+@Setter
+@ToString(callSuper = true)
+@EqualsAndHashCode
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(value = {"hibernateLazyInitializer", "handler", "fieldHandler"}, ignoreUnknown = true)
+public class ReservationResponse implements Response {
+
+    private static final long serialVersionUID = 841299264931794001L;
+
+    @ApiModelProperty(value = "参与者确认资源", required = true)
+    private Participant participantLink;
+
+    public ReservationResponse(Participant participant) {
+        this.participantLink = participant;
+    }
+
+}
